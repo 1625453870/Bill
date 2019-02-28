db2459b5313391faff7950d1d7cef72d74c586e3
zx66 <1625453870@qq.com>
2019-02-28 23:01:33 +0800

    账单类型

---
+++ Code/Bill.Web/Controllers/BillsTypeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bill.Web.Controllers
+{
+    public class BillsTypeController : Controller
+    {
+        // GET: BillsType
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
